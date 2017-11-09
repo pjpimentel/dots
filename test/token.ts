01e32ef398ff2856273fb6fe03104996e7c01ebe
Pedro J. Pimentel <pedro@pimentel.co>
2017-11-09 18:38:11 -0200

    volume

--- test/token.ts
+++ test/token.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 'use strict';
-const token ='7b010d5f2d88a45ddf20aa4f9dd98452089d04c070db233e89d5b433fc2dce95';
+const token ='1250eee7d4e46e3cb58f5006c12840cc9d4d81d84a0f3601a30101bd17f2ec56';
 export default token;
