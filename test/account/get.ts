70608526c3c983d0aec9453e5422509e4dcef650
Pedro J. Pimentel <pedro@pimentel.co>
2018-06-17 17:45:47 -0300

    [rem] removed old test files

--- test/account/get.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import digitalOcean from '../';
-
-let success = data => console.log(data);
-let error = error => console.log(error.message);
-digitalOcean.Account.get().first().subscribe(success, error)
--- test/index.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-import token from './token';
-import DigitalOcean from '../';
-const digitalOcean: DigitalOcean = new DigitalOcean(token);
-export default digitalOcean;
