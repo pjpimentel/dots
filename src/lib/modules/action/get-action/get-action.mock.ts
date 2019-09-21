52f60c89988797a18b983264b4a626f84b3581c6
Pedro J. Pimentel <pedro@pimentel.co>
2019-09-21 13:50:14 -0300

    [add] bkp

---
+++ src/lib/modules/action/get-action/get-action.mock.ts
@@ -0,0 +1,26 @@
+export const request = {
+  "headers": {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer bearer-token"
+  },
+};
+export const response = {
+    "body": {
+      "account": {
+        "droplet_limit": 25,
+        "floating_ip_limit": 5,
+        "email": "sammy@digitalocean.com",
+        "uuid": "b6fr89dbf6d9156cace5f3c78dc9851d957381ef",
+        "email_verified": true,
+        "status": "active",
+        "status_message": ""
+      }
+    },
+    "headers": {
+      "content-type": "application/json; charset=utf-8",
+      "status": 200,
+      "ratelimit-limit": 1200,
+      "ratelimit-remaining": 1137,
+      "ratelimit-reset": 1415984218
+    },
+};
---
+++ src/lib/modules/action/list-actions/list-actions.spec.ts
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { createContext } from '../../../utils';
+import {getAccount} from './get-account';
+import * as MOCK from './list-actions.mock';
+
+describe('account', () => {
+  const URL = '/account';
+  const TOKEN = 'bearer-token';
+  const mock = new MockAdapter(axios);
+  mock.onGet(URL).reply(
+    MOCK.response.headers.status,
+    MOCK.response.body,
+    MOCK.response.headers,
+  );
+  const context = createContext({
+    axios,
+    token: TOKEN,
+  });
+  describe('get-account', () => {
+    it('should be a fn', () => {
+      expect(typeof getAccount).toBe('function');
+    });
+    it('should return a fn', () => {
+      expect(typeof getAccount(context)).toBe('function');
+    });
+    it('should return IResponse<IGetAccountApiResponse>', async () => {
+      const _getAccount = getAccount(context);
+      const response = await _getAccount();
+      Object.assign(response, { request: mock.history.get[0]});
+      /// validate response schema
+      expect(typeof response).toBe('object');
+      expect(typeof response.data).toBe('object');
+      expect(typeof response.headers).toBe('object');
+      expect(typeof response.request).toBe('object');
+      expect(typeof response.status).toBe('number');
+      /// validate request
+      const {request} = response;
+      expect(request.url).toBe(context.endpoint + URL);
+      expect(request.method).toBe('get');
+      expect(request.headers).toMatchObject(MOCK.request.headers);
+      /// validate data
+      expect(response.data).toBeDefined();
+      expect(response.data.account).toBeDefined();
+      const {account} = response.data;
+      expect(typeof account.droplet_limit).toBe('number');
+      expect(typeof account.email_verified).toBe('boolean');
+      expect(typeof account.email).toBe('string');
+      expect(typeof account.floating_ip_limit).toBe('number');
+      expect(typeof account.status_message).toBe('string');
+      expect(typeof account.status).toBe('string');
+      expect(typeof account.uuid).toBe('string');
+      /// validate headers
+      const {headers, status} = response;
+      expect(headers).toMatchObject(MOCK.response.headers);
+      expect(status).toBe(MOCK.response.headers.status);
+    });
+  });
+});
