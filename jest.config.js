45da08fdef7a38338373d69ec9ff93f2d66cb40a
pedro j. pimentel <pedro@pimentel.co>
2019-09-21 15:35:07 -0300

    [add] ignore dist jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   collectCoverage: true,
-  coveragePathIgnorePatterns: ['/node_modules/', '.mock.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', '.mock.ts', 'dist'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '.old'],
+  testPathIgnorePatterns: ['/node_modules/', '.old', 'dist'],
   verbose: true,
 };
