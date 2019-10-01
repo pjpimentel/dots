9c1c6fb4945887243d47316fd3d99abda7f169b0
pedro j. pimentel <pedro@pimentel.co>
2019-09-30 21:39:13 -0300

    [add] list-volume-snapshots

--- src/lib/modules/volume/index.ts
+++ src/lib/modules/volume/index.ts
@@ -1,3 +1,4 @@
 export * from './create-volume/create-volume';
 export * from './get-volume/get-volume';
+export * from './list-volume-snapshots/list-volume-snapshots';
 export * from './list-volumes/list-volumes';
---
+++ src/lib/modules/volume/list-volume-snapshots/list-volume-snapshots.ts
@@ -0,0 +1,25 @@
+import { AxiosInstance } from 'axios';
+
+export interface IListVolumeSnapshotsApiResponse extends IListResponse {
+  snapshots: ISnapshot[];
+}
+
+export interface IListVolumeSnapshotsApiRequest extends IListRequest {
+  id: string;
+}
+
+type ListVolumeSnapshotsRes = IResponse<IListVolumeSnapshotsApiResponse>;
+
+export const listVolumeSnapshots = ({
+  httpClient,
+}: IContext<AxiosInstance>) => async ({
+  id,
+  page = 1,
+  per_page = 25,
+}: IListVolumeSnapshotsApiRequest): Promise<Readonly<ListVolumeSnapshotsRes>> => {
+  const path = `/volumes/${id}/snapshots`;
+  const queryParams = {page, per_page};
+  const url = `${path}`;
+
+  return httpClient.get<IListVolumeSnapshotsApiResponse>(url, {params: queryParams});
+};
