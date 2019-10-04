9b89f8dcffaed20611babc5363379be7a5574329
pedro j. pimentel <pedro@pimentel.co>
2019-10-03 21:33:06 -0300

    [add] create-droplets

---
+++ src/lib/modules/droplet/create-droplets/create-droplets.ts
@@ -0,0 +1,58 @@
+import { AxiosInstance } from 'axios';
+
+export interface ICreateDropletsApiResponse {
+  droplets: IDroplet[];
+}
+
+export interface ICreateDropletsApiRequest {
+  backups?: boolean;
+  image: string | number;
+  ipv6?: boolean;
+  monitoring?: boolean;
+  names: string[];
+  private_networking?: boolean;
+  region: string;
+  size: string;
+  ssh_keys?: number[];
+  tags?: string[];
+  user_data?: string;
+  volumes?: string[];
+}
+
+type CreateDropletsResponse = IResponse<ICreateDropletsApiResponse>;
+
+export const createDroplets = ({
+  httpClient,
+}: IContext<AxiosInstance>) => async ({
+  backups,
+  image,
+  ipv6,
+  monitoring,
+  names,
+  private_networking,
+  region,
+  size,
+  ssh_keys,
+  tags,
+  user_data,
+  volumes,
+}: ICreateDropletsApiRequest): Promise<Readonly<CreateDropletsResponse>> => {
+  const path = '/droplets';
+  const body = {
+    backups,
+    image,
+    ipv6,
+    monitoring,
+    names,
+    private_networking,
+    region,
+    size,
+    ssh_keys,
+    tags,
+    user_data,
+    volumes,
+  };
+  const url = `${path}`;
+
+  return httpClient.post<ICreateDropletsApiResponse>(url, body);
+};
--- src/lib/modules/droplet/index.ts
+++ src/lib/modules/droplet/index.ts
@@ -1,1 +1,2 @@
 export * from './create-droplet/create-droplet';
+export * from './create-droplets/create-droplets';
