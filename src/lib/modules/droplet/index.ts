01dfbb3579356b29cad378e151259ee4a18d86bc
pedro j. pimentel <pedro@pimentel.co>
2019-10-04 22:34:19 -0300

    [add] list-droplet-backups

--- src/lib/modules/droplet/index.ts
+++ src/lib/modules/droplet/index.ts
@@ -1,6 +1,7 @@
 export * from './create-droplet/create-droplet';
 export * from './create-droplets/create-droplets';
 export * from './get-droplet/get-droplet';
+export * from './list-droplet-backups/list-droplet-backups';
 export * from './list-droplet-kernels/list-droplet-kernels';
 export * from './list-droplet-snapshots/list-droplet-snapshots';
 export * from './list-droplets/list-droplets';
---
+++ src/lib/modules/droplet/list-droplet-backups/list-droplet-backups.ts
@@ -0,0 +1,27 @@
+import { AxiosInstance } from 'axios';
+
+export interface IListDropletBackupsApiResponse extends IListResponse {
+  backups: ISnapshot[];
+}
+
+export interface IListDropletBackupsApiRequest extends IListRequest {
+  id: number;
+}
+
+type ListDropletBackupsResponse = IResponse<IListDropletBackupsApiResponse>;
+
+export const listDropletBackups = ({
+  httpClient,
+}: IContext<AxiosInstance>) => async ({
+  id,
+  page = 1,
+  per_page = 25,
+}: IListDropletBackupsApiRequest): Promise<Readonly<ListDropletBackupsResponse>> => {
+  const path = `/droplets/${id}/backups`;
+  const queryParams = {page, per_page};
+  const url = `${path}`;
+
+  return httpClient.get<IListDropletBackupsApiResponse>(url, {
+    params: queryParams,
+  });
+};
