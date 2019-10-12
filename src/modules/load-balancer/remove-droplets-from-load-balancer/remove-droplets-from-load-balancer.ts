6d2566f29208bf0c7e9c5b5b2336df2f5106a999
pedro j. pimentel <pedro@pimentel.co>
2019-10-12 19:38:22 -0300

    [add] remove-droplets-from-load-balancer

---
+++ src/modules/load-balancer/remove-droplets-from-load-balancer/remove-droplets-from-load-balancer.ts
@@ -0,0 +1,23 @@
+import { AxiosInstance } from 'axios';
+
+export interface IRemoveDropletsFromLoadBalancerApiRequest {
+  droplet_ids: number[]
+  id: string;
+}
+
+export type RemoveDropletsFromLoadBalancerResponse = IResponse<void>;
+
+export const removeDropletsFromLoadBalancer = ({
+  httpClient,
+}: IContext<AxiosInstance>) => ({
+  droplet_ids,
+  id,
+}: IRemoveDropletsFromLoadBalancerApiRequest): Promise<Readonly<RemoveDropletsFromLoadBalancerResponse>> => {
+  const path = '/load_balancers';
+  const body = {
+    droplet_ids,
+  };
+  const url = `${path}/${id}`;
+
+  return httpClient.delete(url, {data: body});
+};
