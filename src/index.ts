3af2e0e334a3c35fdb1cc6b1c5ab17f075e2b633
pedro j. pimentel <pedro@pimentel.co>
2019-11-09 16:05:42 -0300

    [ref] export fn

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import * as modules from './modules';
 import * as _utils from './utils';
 
+const createApiClient = _utils.createApiClient({
+  modules,
+  createContext: _utils.createContext,
+});
+
 const utils = {
-  createApiClient: _utils.createApiClient({
-    modules,
-    createContext: _utils.createContext,
-  }),
+  createApiClient,
   createContext: _utils.createContext,
 };
 
-export {modules, utils};
+export {createApiClient, modules, utils};
