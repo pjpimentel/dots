67e3e4e29497d016b68c8aee3dc164e41d399177
pedro j. pimentel <pedro@pimentel.co>
2020-11-21 14:57:44 +0000

    [ref] type

--- src/modules/app/types/app-deployment-progress-step.ts
+++ src/modules/app/types/app-deployment-progress-step.ts
@@ -1,8 +1,13 @@
 import { IAppDeploymentProgressStepReason } from ".";
 
+export type AppDeploymentProgresStepStatus = 'PENDING'
+  | 'RUNNING'
+  | 'ERROR'
+  | 'SUCCESS';
+
 export interface IAppDeploymentProgressStep {
     name:	string;
-    status: 'PENDING' | 'RUNNING' | 'ERROR' | 'SUCCESS'| string;
+    status: AppDeploymentProgresStepStatus | string;
     steps: IAppDeploymentProgressStep[]
     started_at:	string;
     ended_at:	string;
