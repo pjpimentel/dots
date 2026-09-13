e6bfd31f72691422a78f0073e3020a3ca19a722e
pedro jose pimentel <pedro@pimentel.co>
2026-09-13 12:34:29 -0300

    removed tslint

--- src/types/response.ts
+++ src/types/response.ts
@@ -2,6 +2,6 @@
 export interface IResponse<T> {
   data: T;
   status: number;
-  headers: object; // TODO: tslint:disable-line:no-any
-  request?: any; // TODO: tslint:disable-line:no-any
+  headers: object;
+  request?: any;
 }
